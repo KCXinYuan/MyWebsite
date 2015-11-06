c1ef29aa72d346f2dc792b8c20112ca318ba7ee5
Kevin Chuang <kcxinkun@gmail.com>
2015-11-05 18:36:17 -0800

    commit script.js, edit index.html and style.css

---
+++ script.js
@@ -0,0 +1,11 @@
+var secondP = document.getElementById('secondP');
+secondP.innerHTML = 'Hello class, new text here!';
+
+// document.getElementById('firstP').innerHTML = 'I also work';
+
+
+var name = prompt ('What is your name?');
+document.getElementById('userName').innerHTML = name;
+
+var city = prompt ('What city do you live in?');
+document.getElementById('yourCity').innerHTML = "I live in " + city;
